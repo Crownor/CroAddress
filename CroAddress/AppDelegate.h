980b6bda63005cf839515528d1e0eb0024346e61
Departures <departures_egoist@icloud.com>
2016-11-02 14:14:42 +0800

    Initial Commit

---
+++ CroAddress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CroAddress
+//
+//  Created by Departures on 02/11/2016.
+//  Copyright © 2016 Crownor.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CroAddress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CroAddress
+//
+//  Created by Departures on 02/11/2016.
+//  Copyright © 2016 Crownor.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
